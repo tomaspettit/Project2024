7ed5c8c228b95adca7f9344529771b948e4aae61
unknown <G00419414@atu.ie>
2024-04-29 15:18:03 +0000

    Updating

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { RouterLinkWithHref } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { home } from 'ionicons/icons';
+import { logIn } from 'ionicons/icons';
+import { personAdd } from 'ionicons/icons';
+import { card } from 'ionicons/icons';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
   styleUrls: ['./payment.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLinkWithHref]
 })
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({home, logIn, personAdd, card});
+  }
 
   ngOnInit() {
   }
